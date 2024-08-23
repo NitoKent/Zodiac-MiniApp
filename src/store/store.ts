5e945e4a3dda07cc897652b2f37c4ceaaece7a31
Daniel Kirushin <retrostyae@mail.ru>
2024-08-23 17:11:17 +0300

    fixed

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,25 +1,27 @@
-import { configureStore, createSlice, } from '@reduxjs/toolkit';
+import { configureStore, createSlice } from '@reduxjs/toolkit';
 
+// Создание среза состояния для языка
 const languageSlice = createSlice({
   name: 'language',
-  initialState: 'en', 
+  initialState: 'en', // Начальное состояние
   reducers: {
     setLanguage: (state, action) => {
-      
-      return state = action.payload; 
+      // Поскольку Redux Toolkit использует Immer, вы можете изменить состояние напрямую
+      return  state=action.payload; // Обновляем состояние на основе payload
     },
   },
 });
 
 export const { setLanguage } = languageSlice.actions;
 
-
+// Создание хранилища Redux
 const store = configureStore({
   reducer: {
     language: languageSlice.reducer,
   },
 });
 
+// Экспорт типа состояния для использования в компонентах
 export type RootState = ReturnType<typeof store.getState>;
 
 export default store;
