43f6431c492d046fe519160f0f2e291fb9cb10b4
Daniel Kirushin <retrostyae@mail.ru>
2024-08-23 14:49:33 +0300

    .

--- src/cmps/ZodiacList.tsx
+++ src/cmps/ZodiacList.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import '../App.css';
 import libraImage from '../assets/libra.png';
 import taurusImage from '../assets/taurus.png'
